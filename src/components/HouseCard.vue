8f65e699a4d2afa554208037926aa018d81cc9de
Enayatullal Shadab <Enayatullah_Shadab@yahoo.com>
2021-08-05 00:15:12 -0600

    HouseCard completed

---
+++ src/components/HouseCard.vue
@@ -0,0 +1,26 @@
+<template>
+     <div class="house m-2 bg-light shadow">
+        <!-- passing required param ':id' -->
+        <router-link :to="{name: 'HouseDetails', params: {id: house.id}}">
+          <div class="p-3">
+              <div class="text-center">
+                  <p><b> {{house.bedrooms}} - {{house.bathrooms}}-{{house.levels}}- {{house.year}} - {{house.price}}-{{house.imgUrl}}</b></p>
+              </div>
+          </div>
+        </router-link>
+      </div>
+</template>
+
+<script>
+export default {
+    props: {
+        house: {
+            type: Object,
+            required: true
+        }
+         },
+    setup() {
+          return{}
+    },
+}
+</script>
